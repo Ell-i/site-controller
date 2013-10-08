e0b3108f2a65c8ede31f5f48af2f1e963347a37f
Pekka Nikander <pekka.nikander@senseg.com>
2013-10-08 21:02:54 +0300

    First version of lock

--- webux/index.js
+++ webux/index.js
@@ -9,7 +9,11 @@ var handle = {}
 
 handle["/"]               = handlers.file("demo.html");
 handle["/index.html"]     = handlers.file("demo.html");
+handle["/lock.html"]      = handlers.file("lock.html");
 handle["/demo.png"]       = handlers.file("demo.png");
+handle["/lock_open.png"]  = handlers.file("lock_open.png");
+handle["/lock_closed.png"]= handlers.file("lock_closed.png");
 handle["/actuators/leds"] = handlers.coap("coap://" + ip + "/actuators/leds");
+handle["/actuators/lock"] = handlers.coap("coap://" + ip + "/actuators/lock");
 
 server.start(router.route, handle);
