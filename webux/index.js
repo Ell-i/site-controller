6938fff72e9b416ef97466f458bf4e9344c08b99
Pekka Nikander <pekka.nikander@senseg.com>
2013-10-09 10:45:06 +0300

    Redirect leds and lock to different ELL-i

--- webux/index.js
+++ webux/index.js
@@ -3,8 +3,6 @@ var server   = require("./server");
 var router   = require("./router");
 var handlers = require("./handlers");
 
-var ip = "192.168.2.100"
-
 var handle = {}
 
 handle["/"]               = handlers.file("demo.html");
@@ -13,7 +11,7 @@ handle["/lock.html"]      = handlers.file("lock.html");
 handle["/demo.png"]       = handlers.file("demo.png");
 handle["/lock_open.png"]  = handlers.file("lock_open.png");
 handle["/lock_closed.png"]= handlers.file("lock_closed.png");
-handle["/actuators/leds"] = handlers.coap("coap://" + ip + "/actuators/leds");
-handle["/actuators/lock"] = handlers.coap("coap://" + ip + "/actuators/lock");
+handle["/actuators/leds"] = handlers.coap("coap://10.0.0.2/actuators/leds");
+handle["/actuators/lock"] = handlers.coap("coap://10.0.0.3/actuators/lock");
 
 server.start(router.route, handle);
