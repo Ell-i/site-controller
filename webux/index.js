3c32a83dd480c2b18c88d579d95ac25d3d96d5f4
Ivan Raul <supra.material@gmail.com>
2014-11-30 23:12:56 +0200

    Update leds CoAP path

--- webux/index.js
+++ webux/index.js
@@ -11,7 +11,7 @@ handle["/lock.html"]      = handlers.file("lock.html");
 handle["/demo.png"]       = handlers.file("demo.png");
 handle["/lock_open.png"]  = handlers.file("lock_open.png");
 handle["/lock_closed.png"]= handlers.file("lock_closed.png");
-handle["/actuators/leds"] = handlers.coap("coap://10.0.0.2/actuators/leds");
+handle["/actuators/leds"] = handlers.coap("coap://10.0.0.2/leds");
 handle["/actuators/lock"] = handlers.coap("coap://10.0.0.3/actuators/lock");
 
 server.start(router.route, handle);
